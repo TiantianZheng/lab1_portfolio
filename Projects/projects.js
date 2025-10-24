389a55eb04adc535db3498368a29f7aa821d7107
TiantianZheng <t3zheng@ucsd.edu>
2025-10-23 23:58:44 -0700

    add

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,13 +1,25 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 async function initProjects() {
-  const projects = await fetchJSON('../lib/projects.json');
-  const projectsContainer = document.querySelector('.projects');
-  renderProjects(projects, projectsContainer, 'h2');
-  const title = document.querySelector('.projects-title');
-  if (title && projects) {
-    title.textContent = `Projects (${projects.length})`;
+    try {
+     
+      const projects = await fetchJSON('../lib/projects.json');
+  
+    
+      const projectsContainer = document.querySelector('.projects');
+  
+      renderProjects(projects, projectsContainer, 'h2');
+  
+      const title = document.querySelector('.projects-title');
+      if (title && projects) {
+        title.textContent = `Projects (${projects.length})`;
+      }
+  
+      console.log('✅ Projects loaded:', projects);
+    } catch (error) {
+      console.error('❌ Error loading projects:', error);
+    }
   }
-}
-
-initProjects();
+  
+  // 立即执行
+  initProjects()
