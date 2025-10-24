38978dbecbd359dbf7c29560a7c4134195b35efa
TiantianZheng <t3zheng@ucsd.edu>
2025-10-23 23:32:05 -0700

    add projects.js

---
+++ Projects/projects.js
@@ -0,0 +1,13 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+async function initProjects() {
+  const projects = await fetchJSON('../lib/projects.json');
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(projects, projectsContainer, 'h2');
+  const title = document.querySelector('.projects-title');
+  if (title && projects) {
+    title.textContent = `Projects (${projects.length})`;
+  }
+}
+
+initProjects();
