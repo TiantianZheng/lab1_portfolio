31ab3ea4e8a9dbfa147c8d7da6659a93582d4614
TiantianZheng <t3zheng@ucsd.edu>
2025-10-30 20:34:53 -0700

    1

--- Projects/projects.js
+++ Projects/projects.js
@@ -11,37 +11,40 @@ async function initProjects() {
     }
     }
 initProjects();
+
 let svg = d3.select("#projects-pie-plot");
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI
-});
-svg.append("path")
-  .attr("d", arc)
-  .attr("fill", "red");
+// let arc = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI
+// });
+// svg.append("path")
+//   .attr("d", arc)
+//   .attr("fill", "red");
 
-let data = [1, 2];
-
-let total = 0;
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
+// let total = 0;
+// for (let d of data) {
+//   total += d;
+// }
+
+// let angle = 0;
+// let arcData = [];
+
+// for (let d of data) {
+//   let endAngle = angle + (d / total) * 2 * Math.PI;
+//   arcData.push({ startAngle: angle, endAngle });
+//   angle = endAngle;
+// }
+
+// let arcs = arcData.map((d) => arcGenerator(d));
 let colors = ['gold', 'purple'];
 
 arcs.forEach((arc, idx) => {
