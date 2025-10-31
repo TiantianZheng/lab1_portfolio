e21d9237ac9dbf59200f2718ba7448c71e0328b7
TiantianZheng <t3zheng@ucsd.edu>
2025-10-30 19:13:18 -0700

    1

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function initProjects() {
     const projects = await fetchJSON('../lib/projects.json');
@@ -10,3 +11,16 @@ async function initProjects() {
     }
     }
 initProjects();
+let svg = d3.select("#projects-pie-plot");
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+  
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI
+});
+svg.append("path")
+  .attr("d", arc)
+  .attr("fill", "red");
