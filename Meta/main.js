901f155bdaba9cafff9f45ccf209b0e4e17ac003
TiantianZheng <t3zheng@ucsd.edu>
2025-11-06 23:29:32 -0800

    sac

--- Meta/main.js
+++ Meta/main.js
@@ -128,6 +128,19 @@ renderCommitInfo(data, commits);
 function renderScatterPlot(data, commits) {
     const width = 1000;
     const height = 600;
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 }
+
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+      };
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
+      
     const svg = d3
     .select('#chart')
     .append('svg')
@@ -141,6 +154,25 @@ function renderScatterPlot(data, commits) {
     .nice();
 
     const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+    
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale)
+    .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add X axis
+    svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    // Add Y axis
+    svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+    
     const dots = svg.append('g').attr('class', 'dots');
 
     dots
