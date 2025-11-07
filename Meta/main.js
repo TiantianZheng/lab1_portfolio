94ba039340db704724b78bc749b0a61c7b631561
TiantianZheng <t3zheng@ucsd.edu>
2025-11-06 23:41:26 -0800

    1

--- Meta/main.js
+++ Meta/main.js
@@ -166,7 +166,9 @@ function renderScatterPlot(data, commits) {
     );
 
 
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale)
+    .ticks(d3.timeWeek.every(1))
+    .tickFormat(d3.timeFormat("%b %d"));
     const yAxis = d3.axisLeft(yScale)
     .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
 
