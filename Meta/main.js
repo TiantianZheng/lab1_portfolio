4e1a81d56280694f3d449c7f1b741cdce80e6050
TiantianZheng <t3zheng@ucsd.edu>
2025-11-06 19:39:54 -0800

    change github

--- Meta/main.js
+++ Meta/main.js
@@ -11,8 +11,36 @@ async function loadData() {
     console.log(data);
     return data;
   }
-
   
+function processCommits(data) {
+return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+    let first = lines[0];
+    let { author, date, time, timezone, datetime } = first;
+    let ret = {
+        id: commit,
+        url: 'https://github.com/TiantianZheng/lab1_portfolio/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+    };
+
+    Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
+        configurable: true,
+      });
+  
+
+    return ret;
+    });
+}
+
   
   
 let data = await loadData();
